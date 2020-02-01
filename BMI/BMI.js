e40fd8299be6375f18eb8ea1f7a47e32a1bcc672
Hank0725 <hank2929@hotmail.com>
2020-02-01 19:35:15 +0800

    2020v1

--- BMI/BMI.js
+++ BMI/BMI.js
@@ -1,44 +1,65 @@
 const btn  = document.querySelector(".button");
 const list = document.querySelector(".list");
-const OutData =  [];
+const circle = document.querySelector(".circle");
+const reset = document.querySelector(".smallcircle");
+const result = document.querySelector(".result");
+const resultcircyle = document.querySelector(".resultcircyle");
+const inputKg = document.querySelector(".inputKg");
+const inputCm = document.querySelector(".inputCm");
+const OutData = JSON.parse(localStorage.getItem('LocalData')) || [];
 
 //監聽btn執行函式
 btn.addEventListener("click",Cal,false);
 list.addEventListener("click",ToDel,false);
+reset.addEventListener("click",Reset,false);
 //使用函式計算BMI數值
 function Cal(e) {
   e.preventDefault();
+  let str  = "";
   const Kg   = document.querySelector(".inputKg").value;
   const Cm   = document.querySelector(".inputCm").value;
   let BMI  = (Kg/(Cm/100*Cm/100));
   BMI = BMI.toFixed(2);
+  if(Kg === "" || Cm === ""  ){
+    alert("請輸入身高和體重數字"); 
+    return;}
+  
   if (BMI <= 18.5) {
     status = "過輕"
+    str += '<div class="result" style="display:block" id="circlelightfat"><p class="circyleBMI">'+BMI+'</br>BMI值'+'<p><div class="smallcircle"><a href=""><img src="https://github.com/Hank0725/BMI/blob/master/BMI/icons_loop.png?raw=true"></a></div></div><p class="outside">'+status+'</p>';
   }
   else if(BMI > 18.5 && BMI <= 24){
     status = "正常範圍"
+    str += '<div class="result" style="display:block" id="circleideafat"><p class="circyleBMI">'+BMI+'</br>BMI值'+'<p><div class="smallcircle"><a href=""><img src="https://github.com/Hank0725/BMI/blob/master/BMI/icons_loop.png?raw=true"></a></div></div><p class="outside">'+status+'</p>';
   } 
   else if(BMI > 24 && BMI <= 27  ){
     status = "過重"
+    str += '<div class="result" style="display:block" id="circletoofat"><p class="circyleBMI">'+BMI+'</br>BMI值'+'<p><div class="smallcircle"><a href=""><img src="https://github.com/Hank0725/BMI/blob/master/BMI/icons_loop.png?raw=true"></a></div></div><p class="outside">'+status+'</p>';
   } 
   else if(BMI > 27 && BMI <= 30){
     status = "輕度肥胖"
+    str += '<div class="result" style="display:block" id="circleveryfat"><p class="circyleBMI">'+BMI+'</br>BMI值'+'<p><div class="smallcircle"><a href=""><img src="https://github.com/Hank0725/BMI/blob/master/BMI/icons_loop.png?raw=true"></a></div></div><p class="outside">'+status+'</p>';
   } 
   else if(BMI > 30 && BMI <= 35){
     status = "中度肥胖"
+    str += '<div class="result" style="display:block" id="circlesuperfat"><p class="circyleBMI">'+BMI+'</br>BMI值'+'<p><div class="smallcircle"><a href=""><img src="https://github.com/Hank0725/BMI/blob/master/BMI/icons_loop.png?raw=true"></a></div></div><p class="outside">'+status+'</p>';
   } 
   else if(BMI > 35){
     status = "重度肥胖"
+    str += '<div class="result" style="display:block" id="circlelbigfat"><p class="circyleBMI">'+BMI+'</br>BMI值'+'<p><div class="smallcircle"><a href=""><img src="https://github.com/Hank0725/BMI/blob/master/BMI/icons_loop.png?raw=true"></a></div></div><p class="outside">'+status+'</p>';
   } 
+  
   var data = {
     height:Cm,
     weight:Kg,
     bmi:BMI,
     status:status};
-  OutData.push(data);
+  
+  OutData.push(data); 
   update(OutData);
   localStorage.setItem('LocalData',JSON.stringify(OutData)); 
-  
+  resultcircyle.innerHTML = str ;
+  changeBtn();
  }
 
 //利用函式列出BMI各種數值
@@ -124,3 +145,21 @@ function ToDel(e){
   update(OutData);
 }
 
+/*----------  更新按鈕  ----------*/
+function changeBtn() {
+  btn.style.display = 'none';
+  result.style.display =  'block';
+}
+
+
+/*----------  清空，重設輸入內容  ----------*/
+function Reset() {
+  circle.style.display = 'block';
+  result.removeAttribute('class');
+  circyleBMI.removeAttribute('class');
+  smallcircle.removeAttribute('class');
+  outside.removeAttribute('class');
+  
+  inputKg.value = '';
+  inputCm.value = '';
+}
